ed57332170ca9e4ea4f15dca00f50e9c4399905b
syahdansaja <bosjerukasem@gmail.com>
2025-05-14 18:23:12 +0700

    Fifth commit

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,12 +2,34 @@ import Navlink from "../components/Navlink";
 import { Link } from "react-router";
 import ReactIcon from "../assets/images/react.svg";
 import tailwindCssIcon from "../assets/images/tailwindcss.svg";
+import { useEffect, useState } from "react";
 
 
 export default function MainLayout({children}) {
+    const [visible, setVisible] = useState(true);
+    const [initialLoad, setInitialLoad] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    function handleScroll () {
+      const currentScrollY = window.scrollY;
+      if(currentScrollY < lastScrollY) {
+        setVisible(true);
+      } else if (currentScrollY > 50) {
+        setVisible(false);
+      }
+      setLastScrollY(currentScrollY);
+    }
+    useEffect(() => {
+      const timeout = setTimeout(() => setInitialLoad(false), 100);
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        clearTimeout(timeout);
+        window.removeEventListener('scroll', handleScroll);
+      }
+    }, [lastScrollY]);
     return (
         <>
-         <nav className="flex justify-between fixed top-0 left-0 right-0 items-center px-6 py-4 bg-none z-50">
+         <nav className={`navbar flex justify-between fixed top-0 left-0 right-0 w-full items-center px-6 py-4 bg-white z-50 transition-opacity duration-500 ease-in-out ${!visible ? 'opacity-0 pointer-events-none' : 'opacity-65'} ${initialLoad ? 'opacity-0' : ''}`}>
            <Link className="title" to="/" >syahdanisme</Link>
            <div className="flex gap-10">
             <Navlink to="/">Home</Navlink>
@@ -17,10 +39,10 @@ export default function MainLayout({children}) {
            </div>
          </nav>
          {children}
-         <footer className="px-6 fixed bottom-0 w-screen">
+         <footer className="px-6 w-screen bottom-0 bg-gray-700">
           <div className="flex justify-between w-full">
-            <span className="text-sm font-extralight text-black">copyright © 2025 , Syahdan</span>
-            <span className="text-sm font-extralight text-black flex gap-2" style={{alignItems:"center"}}>
+            <span className="text-sm font-extralight text-white">copyright © 2025 , Syahdan</span>
+            <span className="text-sm font-extralight text-white flex gap-2" style={{alignItems:"center"}}>
               <span>Powered by React</span>
               <img rel="icon" src={ReactIcon} width={"20px"} height={"20px"} />
               <span>, Tailwindcss</span>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import MainLayout from "../layouts/MainLayout";
 
 export default function Home() {
+
     return (
         <MainLayout>
-            <section className="py-48 px-48 w-full h-[200vh] bg-sky-100 flex justify-between">
-                <div className="w-[50%]">
+            <section className="py-48 px-28 w-full h-[300vh] bg-sky-100 flex justify-between">
+                <div className="w-[50%] h-full justify-items-center align-middle" style={{ alignContent: "center" }}>
                     <h1 className="text-4xl font-extrabold bubblegum">Hello, I'm Syahdan</h1>
                 </div>
+                <div className="w-[50%] h-full justify-items-center align-middle">
+                    
+                </div>
+            </section>
+            <section>
+
             </section>
         </MainLayout>
     )
